1cca5a82fc3e146ba0726c8a405399029183f00e
hyunsu <heonsuu159@naver.com>
2022-12-17 12:14:02 +0900

    test test

--- quiz/tests.py
+++ quiz/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django import views
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from .views import helloAPI
 
-# Create your tests here.
+class hello(APITestCase):
+    
+    def test_hello(self):
+        response = self.client.get(reverse('hello'))
+        print(response)
+        print(response.data)
+            
+        #self.assertEqual(response.status_code,status.HTTP_200_OK)
--- quiz/urls.py
+++ quiz/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from .views import helloAPI, randomQuiz, byeAPI
 
 urlpatterns = [
-    path("hello/", helloAPI),
+    path("hello/", helloAPI,name='hello'),
     path("<int:id>/", randomQuiz),
     path("bye/", byeAPI),
 ]
